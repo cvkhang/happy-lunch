78ad6b437701a21524c81ab84ef327afacf58f5e
cvkhang <chu.v.khang@gmail.com>
2025-12-17 17:13:35 +0700

    fix: increase pool limit

--- backend/config/database.js
+++ backend/config/database.js
@@ -16,7 +16,7 @@ if (process.env.DATABASE_URL) {
       }
     },
     pool: {
-      max: 10,
+      max: 100,
       min: 0,
       acquire: 60000,
       idle: 10000
@@ -32,7 +32,7 @@ if (process.env.DATABASE_URL) {
       dialect: process.env.DB_DIALECT,
       logging: false,
       pool: {
-        max: 10,
+        max: 100,
         min: 0,
         acquire: 60000,
         idle: 10000
